7bcd3e2cd5a099a5db0230668517dac90f25fc90
Testy <SikoraTomaszIT@gmail.com>
2018-07-02 19:05:43 +0200

    code optimization

--- scripts.js
+++ scripts.js
@@ -1,48 +1,28 @@
-document.addEventListener("DOMContentLoaded",function(){
-
-    document.addEventListener('keydown',function(e){
+document.addEventListener("DOMContentLoaded", function() {
+    
+    function generateGradient(e) {
         if (e.which == '32') {
             const body = document.querySelector('body');
+
             function getRandomColor() {
-              var letters = '0123456789ABCDEF';
-              var color = '#';
-              for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-              }
-              return color;
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
             }
-            function getAngle(){
+
+            function getAngle() {
                 let number = Math.floor((Math.random() * 360) + 1);
                 return number;
             }
-            getAngle();
             body.style.setProperty('--firstGradientColor', getRandomColor());
             body.style.setProperty('--secondGradientColor', getRandomColor());
-            body.style.setProperty('--angle', getAngle()+'deg');
+            body.style.setProperty('--angle', getAngle() + 'deg');
         }
-
-    })
-    document.addEventListener('touchend',function(e){
-        
-            const body = document.querySelector('body');
-            function getRandomColor() {
-              var letters = '0123456789ABCDEF';
-              var color = '#';
-              for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-              }
-              return color;
-            }
-            function getAngle(){
-                let number = Math.floor((Math.random() * 360) + 1);
-                return number;
-            }
-            getAngle();
-            body.style.setProperty('--firstGradientColor', getRandomColor());
-            body.style.setProperty('--secondGradientColor', getRandomColor());
-            body.style.setProperty('--angle', getAngle()+'deg');
-
-
-    })
+    }
+    document.addEventListener('keydown', generateGradient);
+    document.addEventListener('touchend', generateGradient);
 
 })
